4a3f921f1522bddd65e9eb3f7b021ec896a29f46
deepak5230 <151017553+deepak5230@users.noreply.github.com>
2024-05-31 14:32:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function insert_Row() {
-    //Write your code here
+        const table = document.getElementById('sampleTable');
+    
+    // Create a new row and two new cells
+    const newRow = table.insertRow(0); // Insert at the top (index 0)
+    const newCell1 = newRow.insertCell(0);
+    const newCell2 = newRow.insertCell(1);
+    
+    // Set the text for the new cells
+    newCell1.textContent = 'New Cell1';
+    newCell2.textContent = 'New Cell2';
+
   
   
 }
